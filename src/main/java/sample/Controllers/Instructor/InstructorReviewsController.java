aa0431cdfd70d5ba91699158af6c6a77a71b22e1
stelian65 <v.stely71@gmail.com>
2021-05-13 14:43:15 +0300

    SLA-26 SLA-19

--- src/main/java/sample/Controllers/Instructor/InstructorReviewsController.java
+++ src/main/java/sample/Controllers/Instructor/InstructorReviewsController.java
@@ -1,4 +1,40 @@
 package sample.Controllers.Instructor;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.scene.Parent;
+import org.w3c.dom.Text;
+import sample.services.UserService;
+import sample.users.User;
+
+import javax.validation.constraints.Null;
+import java.io.IOException;
 
 public class InstructorReviewsController {
+
+
+    @FXML
+    private TextField nume;
+    @FXML
+    private Label eroare;
+    @FXML
+     private ListView list =new ListView<String>();
+    private ObservableList<String> items = FXCollections.observableArrayList();
+
+
+    @FXML
+    public void CheckButtonOnAction(ActionEvent event) throws IOException{
+        String n=nume.getText();
+        items.add(UserService.returnsReviews(n));
+
+            list.setItems(items);
+
+
+    }
 }
